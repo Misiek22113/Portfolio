6627671452b0d644dd134b9e7c9964242e6fb681
Misiek22113 <97341476+Misiek22113@users.noreply.github.com>
2023-03-22 22:19:11 +0100

    add navbar, set navbar grid

---
+++ app/components/navbar/navbar.tsx
@@ -0,0 +1,16 @@
+import "./Navbar.scss";
+
+const Navbar = () => {
+  return (
+    <div className="navbar-wrapper">
+      <div className="name-container">
+        <h1 className="name-text">Jakub Miśko</h1>
+      </div>
+      <div className="options-container"></div>
+      <div className="social-media-container"></div>
+      <div className="contact-container"></div>
+    </div>
+  );
+};
+
+export default Navbar;
