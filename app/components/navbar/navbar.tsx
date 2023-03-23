af7f81e6999bba31b0a68ed0621e7b22d2a8f5f0
Misiek22113 <97341476+Misiek22113@users.noreply.github.com>
2023-03-23 22:47:59 +0100

    start mobile navbar

--- app/components/navbar/navbar.tsx
+++ app/components/navbar/navbar.tsx
@@ -1,24 +1,58 @@
+"use client";
 import "./Navbar.scss";
 import GITHUB from "../../../public/navabr-icons/github.svg";
 import LINKEDIN from "../../../public/navabr-icons/linkedin.svg";
 import Image from "next/image";
+import { useState } from "react";
 
 const Navbar = () => {
+  const [burgerVisible, setburgerVisible] = useState(true);
+
+  const handleBurgerClick = () => {
+    setburgerVisible(!burgerVisible);
+  };
+
   return (
     <div className="navbar-wrapper">
-      <div className="name-container">
+      <div className="name-container-mobile">
         <h1 className="name-text">JAKUB MIŚKO</h1>
       </div>
-      <div className="options-container">
-        <h1 id="work-text">WORK</h1>
-        <h1 id="about-text">ABOUT</h1>
-      </div>
-      <div className="social-media-container">
-        <Image src={GITHUB} className="icon" alt="wykurwiaj"></Image>
-        <Image src={LINKEDIN} className="icon" alt="wykurwiaj"></Image>
-      </div>
-      <div className="contact-container">
-        <h1 id="contact-text">CONTACT</h1>
+      <button
+        onClick={handleBurgerClick}
+        className={
+          burgerVisible == true ? "toggle-button-burger" : "toggle-button-close"
+        }
+        id="burger-button"
+      ></button>
+      <div className="navbar-list" data-is-active={`${burgerVisible}`}>
+        <div className="name-container">
+          <h1 className="name-text">JAKUB MIŚKO</h1>
+        </div>
+        <div className="options-container">
+          <h1 id="work-text">WORK</h1>
+        </div>
+        <div className="about-container">
+          <h1 id="about-text">ABOUT</h1>
+        </div>
+        <div className="social-media-container">
+          <Image
+            src={GITHUB}
+            className="icon"
+            alt="nothing"
+            width={2}
+            height={3}
+          ></Image>
+          <Image
+            src={LINKEDIN}
+            className="icon"
+            alt="nothing"
+            width={3}
+            height={3}
+          ></Image>
+        </div>
+        <div className="contact-container">
+          <h1 id="contact-text">CONTACT</h1>
+        </div>
       </div>
     </div>
   );
